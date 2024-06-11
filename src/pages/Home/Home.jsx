f2e727947abdd8c784ef4601ce24cf72f41f3cbb
Javier-Rivera2311 <142764215+Javier-Rivera2311@users.noreply.github.com>
2024-06-11 18:25:31 -0400

    style(home): mejoras grid

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import PageContainer from "../../components/container/PageContainer";
 import { Box, Typography, Button, Grid } from "@mui/material";
 import img from "../../assets/3802449.png";
@@ -12,15 +13,14 @@ function Home() {
           justifyContent: "center",
           alignItems: "center",
           width: "100%",
-          minHeight: "calc(80vh - 64px)", // Ajusta la altura para tener en cuenta el header y footer
+          minHeight: "calc(60vh - 50px)", // Ajusta la altura para tener en cuenta el header y footer
           backgroundColor: "black",
           color: "white",
-          padding: 2,
         }}
       >
         <Grid container spacing={2}>
           <Grid item xs={12} md={6}>
-            <Box padding={7}>
+            <Box padding={6}>
               <Typography
                 variant="h2"
                 component="h1"
@@ -49,25 +49,28 @@ function Home() {
               >
                 Planifica y mide la activación muscular en tus entrenamientos con los sensores EMG usando FitSense y mejora tu rendimiento.
               </Typography>
-              <Button
-                variant="contained"
-                sx={{
-                  backgroundColor: "orange",
-                  color: "black",
-                  fontWeight: "bold",
-                  padding: "10px 20px",
-                  fontSize: "1.2rem",
-                  marginTop: 2,
-                  display: 'grid',
-                  justify: 'center',
-                  marginLeft: 20,
-                  "&:hover": {
-                    backgroundColor: "darkorange",
-                  },
-                }}
-              >
-                MIS ENTRENAMIENTOS
-              </Button>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <Link to="/Workout" style={{ textDecoration: 'none', display: 'inline-block' }}>
+                <Button
+                  variant="contained"
+                  sx={{
+                    backgroundColor: "orange",
+                    color: "black",
+                    fontWeight: "bold",
+                    padding: "10px 20px",
+                    fontSize: "1.2rem",
+                    marginTop: 2,
+                    display: 'grid',
+                    justify: 'center',
+                    "&:hover": {
+                      backgroundColor: "darkorange",
+                    },
+                  }}
+                >
+                  MIS ENTRENAMIENTOS
+                </Button>
+              </Link>
+            </div>
             </Box>
           </Grid>
           <Grid item xs={12} md={5}>
